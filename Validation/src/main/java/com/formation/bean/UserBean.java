8115a940ede62e46dc57101188cd8882f90843ca
thomasbvn <54573013+thomasbvn@users.noreply.github.com>
2019-09-14 00:39:16 +0200

    h

---
+++ Validation/src/main/java/com/formation/bean/UserBean.java
@@ -0,0 +1,40 @@
+package com.formation.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UserBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private int age;
+
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "UserBean [userName=" + userName + ", age=" + age + "]";
+	}
+	
+}
